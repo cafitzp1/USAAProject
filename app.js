6cf3f3bc04103df5ff1c091dfb2ba8f32fdc4fed
Raajpaal <rgohlwar@asu.edu>
2019-03-17 10:44:33 -0700

    Added index.html file. Modified app.js file to be able to accept html files and serve static website.

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var http = require('http');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Raaj: Commented out these lines of code
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -39,3 +41,8 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+// Raaj: added these lines of code to serve static website
+// serve static website using html files
+app.use(express.static(__dirname + '/public'));
+http.createServer(app).listen(3000);
