617c5f017147cdf50d27562d6cb7e72882c0e4a4
Connor <cafitzp1@asu.edu>
2019-03-26 15:08:47 -0700

    Added db connectivity and ejs view template

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var http = require('http');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const http = require('http');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const loginRouter = require('./routes/login');
+const feedRouter = require('./routes/feed');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,7 +18,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/feed', feedRouter);
+
+// set view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,6 +43,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-// serve static website using html files
-app.use(express.static(__dirname + '/public'));
---
+++ routes/login.js
@@ -0,0 +1,53 @@
+const dbHelp = require('../lib/dbHelp');
+const express = require('express');
+const router = express.Router();
+
+// GET login page
+router.get('/', (req, res, next) => {
+    res.render('login');
+});
+
+// POST authorization
+router.post('/', (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    console.log(email + " : " + password);
+
+    //let sql = `CALL SystemUser_Get(?, ?)`;
+    let sql = `
+        SELECT      *
+        FROM	    SystemUser
+        WHERE       Email = '${email}' AND Password = '${password}';
+    `;
+    let conn = dbHelp.dbConnection(3000);
+
+    conn.connect();
+    //conn.query(sql, [email, password], (error, results) => {
+    conn.query(sql, (error, results) => {
+        if (error) {
+            console.error(error);
+            res.render('login');
+        } else {
+            console.log('results: ' + results);
+
+            // end connection
+            conn.end((err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+            // route to home page if results
+            if (results.length > 0) {
+                console.log('success');
+                res.render('home');
+            } else {
+                console.log('fail');
+                //res.render('login', {error: 'Invalid user credentials'});
+                res.render('login');
+            }
+        }
+    });
+})
+
+module.exports = router;
