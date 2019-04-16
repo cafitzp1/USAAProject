eb3a336c6f96f9e039d82b198fbd06765333b06c
Connor <cafitzp1@asu.edu>
2019-04-15 18:16:00 -0700

    Fixed button toggling, post data now required before submitting

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,62 +2,6 @@
 
 //--- METHODS ---//
 
-const createPost = () => {
-
-    // store inputs into json object
-    let data = {
-        postTitle: $('#post-title').val(),
-        postBody: $('#post-body').val(),
-        postAuthor: $('#post-author').val(),
-        datePosted: (new Date(Date.now())).toLocaleString()
-    };
-
-    // return if missing fields
-    if (data.postTitle == "" || data.postBody == "" || data.postAuthor == "") {
-        window.alert('Missing one or more field(s)');
-        return;
-    }
-
-    // disable, show loading
-    $('#post-button').attr('disabled', true);
-    $('#post-button').html('<span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>');
-
-    // // store in db
-    // $('#post-form').submit((event) => {
-    //     event.preventDefault(); // Stops browser from navigating away from page
-    //     $.post('/new-post', data, (resp) => { // FIXME: cannot find /new-post from this directory
-    //         // success
-    //         console.log('succes');
-
-    //         displayPost(postTitle, postBody, postAuthor, datePosted);
-
-    //         // clear post area
-    //         $('#post-title').val("");
-    //         $('#post-body').val("");
-    //         $('#post-author').val("");
-
-    //         // reset button
-    //         $('#post-button').attr('disabled', false);
-    //         $('#post-button').html('Post');
-    //     }).fail( (error) => {
-    //         // error
-    //         alert(error);
-    //     })
-    // });
-
-    // success
-    displayPost(data.postTitle, data.postBody, data.postAuthor, data.datePosted);
-
-    // clear post area
-    $('#post-title').val("");
-    $('#post-body').val("");
-    $('#post-author').val("");
-
-    // reset button
-    $('#post-button').attr('disabled', false);
-    $('#post-button').html('Post');
-};
-
 const displayPost = (postTitle, postBody, postAuthor, datePosted) => {
 
     // set name to anonymous if undefined
@@ -103,22 +47,17 @@ const displayPostPlaceholder = () => {
 
     let postBody = document.getElementById("post-body");
 
-    if (document.getElementById("suggestions-radio").checked)
-    {
+    if (document.getElementById("suggestions-radio").checked) {
         postBody.placeholder = "What is the issue?\n\nHow do you suggest we fix it?";
     }
-    if (document.getElementById("grievances-radio").checked)
-    {
+    if (document.getElementById("grievances-radio").checked) {
         postBody.placeholder = "What is the issue?\n\nWhat is the impact?\n\nHow should we address this issue?";
     }
-    if (document.getElementById("praise-radio").checked)
-    {
+    if (document.getElementById("praise-radio").checked) {
         postBody.placeholder = "Whose work are you praising?\n\nWhat did they do?\n\nHow did it have a meaningful impact?";
     }
-    if (document.getElementById("announcements-radio") != null)
-    {
-        if (document.getElementById("announcements-radio").checked)
-        {
+    if (document.getElementById("announcements-radio") != null) {
+        if (document.getElementById("announcements-radio").checked) {
             postBody.placeholder = "What is the announcement?";
         }
     }
@@ -127,27 +66,24 @@ const displayPostPlaceholder = () => {
 
 //--- EVENT HANDLERS ---//
 
-$('#post-button').click(() => {
-    //createPost();
-});
-
 $('.radioBtn').click(() => {
     displayPostPlaceholder();
 });
 
 // equivalent of an on load method
-$( () => {
+$(() => {
     // initialize tooltips
     $('[data-toggle="tooltip"]').tooltip()
 
     // execute code based on class attribute applied to body element
-    if ($('body.has-feed').length > 0)
-    {
-        // ...
+    if ($('body.has-feed').length > 0) {
+        $('#home-nav-item').removeClass('active');
+        $('#forum-nav-item').addClass('active');
+    } else if ($('body.has-home').length > 0) {
+        $('#forum-nav-item').removeClass('active');
+        $('#home-nav-item').addClass('active');
     }
 });
 
 // TODO: implement handler for region and role select onchange to filter data
-// TODO: require all data for new post
-// TODO: fix anonymous forum view button and forum nav item toggling when selected
 // TODO: add separate input elements for post radio options, concatenate on post
